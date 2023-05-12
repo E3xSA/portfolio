0d02701a854b82d57e0a85c21c02d4477464a24e
Saboor Shawkat <70259103+Saboor-Shawkat@users.noreply.github.com>
2023-05-12 13:32:54 +0530

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Bhat Aasim";
+            document.title = "Portfolio | Saboor Shawkat";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
